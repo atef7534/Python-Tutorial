8d6cb08b8d9322ed75b74f2e5647384df287b5cf
atif_7534 <77503367+atef7534@users.noreply.github.com>
2026-02-17 16:59:26 +0200

    Uploaded new Material

---
+++ Strings Indexing And Slicing/index.py
@@ -0,0 +1,18 @@
+myStringOne = "I love Python. I hate Java."
+
+print(myStringOne)
+print(myStringOne[0])
+print(len(myStringOne))
+print(myStringOne[-1])
+print(myStringOne[-14 // 2])
+print(myStringOne[14 // 2])
+
+
+myStringOne = "I love Python. I hate Java."
+print(myStringOne[2] + myStringOne[3] + myStringOne[4] + myStringOne[5])
+print(myStringOne[2: 6])
+print(myStringOne[-10: ])
+print(myStringOne[:])
+print(myStringOne[3: ])
+print(myStringOne[0::1])
+print(myStringOne[::2])
