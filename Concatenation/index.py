73d711715d6b85577d499c15e97f8aa98837044c
atif_7534 <77503367+atef7534@users.noreply.github.com>
2026-02-18 17:03:00 +0200

    Uploading new lessons

--- Concatenation/index.py
+++ Concatenation/index.py
@@ -1,12 +1,25 @@
-msg = "Hello"
-name = "Mohamed!"
+msg = "I love"
+lang = "Python"
 
-print(msg, name)
-print(msg + name)
-print(msg + " " + name)
+print(msg + lang + " ❌")
+print(msg + ' ' + lang + " ✅")
 
-aboutme = "My name \
-is Atif. \
-I am 24 years old.\
-"
-print(aboutme)
+result = msg + ' ' + lang + " ✅"
+print(result)
+
+mystr = "My name is Atif. \
+I am 24 years old. \
+I am a recent graduate."
+print(mystr)
+
+a = "I love Python ✅"
+b = "I lovePython ❌"
+
+# "I love Python ✅\nI lovePython ❌"
+print(a + "\n" + b)
+print(a + "\n") 
+
+
+print("Hello " + str(1)) # Error 
+
+# str(1) = "1"
---
+++ Strings/index.py
@@ -0,0 +1,16 @@
+myStringOne = 'This is a single quote'
+myStringTwo = "This is a double quote"
+
+print(myStringOne)
+print(myStringTwo)
+
+myName = "My name is 'Atif'."
+myFriendName = "My friend name is \"Mohamed\""
+
+print(myName)
+print(myFriendName)
+
+myStringThree = '''
+    Have a nice day! 🎉   
+'''
+print(myStringThree)
