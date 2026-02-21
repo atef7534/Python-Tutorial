93210e2b192da22f12143a2c6372c40a184ee543
atif_7534 <77503367+atef7534@users.noreply.github.com>
2026-02-21 16:55:10 +0200

    Changing a folder name

---
+++ Challenges/Challenge-02/index.py
@@ -0,0 +1,24 @@
+msg = "I Love Python And Programming"
+
+# 📌 Tasks
+# Print the first letter
+print(msg[0])
+
+# Print the last letter
+print(msg[-1])
+
+# Print the word Python
+print(msg[7: 14])
+
+# Print the string reversed
+print(msg[::-1])
+
+# Print the number of characters
+print(len(msg))
+
+# Convert the string to:
+# All uppercase
+print(msg.upper())
+
+# All lowercase
+print(msg.lower())
